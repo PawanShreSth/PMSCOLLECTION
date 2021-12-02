79e69868b4a057f7cc77c4b90c50aff565bbafc8
Pawan Shrestha <pawanshresthait@gmail.com>
2021-12-02 09:10:32 +0545

    Setting up few routes to test

---
+++ ecommerce-server-side/product-data/products.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Compton Gray Cap',
+    image: '/images/compton_cap.jpg',
+    description:
+      'The Great Compton is represented here in this awesome one of a kind Compton baseball cap with raised 3D Embroidery. Adjustable snap back closure. One Size Fits Most.',
+    category: 'Cap',
+    price: 800,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Brown Jacket',
+    image: '/images/brown_jacket.jpg',
+    description:
+      '',
+    category: 'Clothes',
+    price: 2000,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Illuminati Jacket',
+    image: '/images/illuminati.jpg',
+    description:
+      '',
+    category: 'Clothes',
+    price: 2500,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Black Plain Adidas Jacket',
+    image: '/images/adidas_jacket.jpg',
+    description:
+      '',
+    category: 'Clothes',
+    price: 2500,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Jacket with stickers',
+    image: '/images/jacket_with_stickers.jpg',
+    description:
+      '',
+    category: 'Clothes',
+    price: 3500,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Nike Bag',
+    image: '/images/nike_bag.jpg',
+    description:
+      '',
+    category: 'Bag',
+    price: 1500,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '7',
+    name: 'Sports Bag',
+    image: '/images/sportsbag.jpg',
+    description:
+      '',
+    category: 'Bag',
+    price: 1700,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '8',
+    name: 'Trousers',
+    image: '/images/trousers.jpg',
+    description:
+      '',
+    category: 'Clothes',
+    price: 1100,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+module.exports = products
