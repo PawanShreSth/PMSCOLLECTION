00f9b9fbf7ab5b2ef4e9673a5bea6dd869e7f310
Pawan Shrestha <pawanshresthait@gmail.com>
2022-01-17 06:11:22 +0545

    Add  Mongoose and Connecting to database

--- ecommerce-server-side/server.js
+++ ecommerce-server-side/server.js
@@ -1,20 +1,26 @@
 import express from 'express';
 import products from './product-data/products.js';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
-dotenv.config()
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+  res.json(products);
+});
 
 app.get('/api/products/:id', (req, res) => {
-    const product = products.find( product => product._id === req.params.id);
-    res.json(product)
-})
+  const product = products.find(product => product._id === req.params.id);
+  res.json(product);
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Listening on Port ${PORT} | Mode = ${process.env.NODE_ENV}`));    
+app.listen(
+  PORT,
+  console.log(`Listening on Port ${PORT} | Mode = ${process.env.NODE_ENV}`)
+);
